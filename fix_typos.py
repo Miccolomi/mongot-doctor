bd2465d353bc5f4756365dae31256a606980aa10
Michele Farinacci <michele.farinacci@m-m9h27477xk.homenet.telecomitalia.it>
2026-03-09 17:52:51 +0100

    n

--- fix_typos.py
+++
@@ -1,9 +0,0 @@
-with open("mongot_monitor.py", "r") as f:
-    text = f.read()
-
-text = text.replace("Errhours", "Error")
-text = text.replace("chours", "core")
-text = text.replace("Chours", "Cores")
-
-with open("mongot_monitor.py", "w") as f:
-    f.write(text)
